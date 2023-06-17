dbd0ed3ec0cbe3fc42e59f4d6d35b00b4476d271
maniolos <maniolos8182003@gmail.com>
2023-06-17 18:51:47 +0200

    portfolio start

---
+++ app/Projects/page.tsx
@@ -0,0 +1,9 @@
+export default function Projects() {
+    return <>
+    <header className="flex flex-wrap h-20 content-center text-3xl text-center justify-center ">Nothin here yet buddy</header>
+    
+    <div className="flex flex-wrap h-50 content-center justify-center"><a href="/" className="rounded-lg px-3 py-2 text-slate-200 font-medium hover:bg-slate-100 hover:text-slate-900">Back</a></div>
+    
+    
+    </>
+}
---
+++ app/portfolio/scripts.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useRef } from 'react';
+import styles from './animations.css';
+
+const SlidingWords = ({ words }) => {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const container = containerRef.current;
+    const wordElements = container.getElementsByClassName('word');
+
+    const animateWords = () => {
+      let delay = 0;
+      Array.from(wordElements).forEach((wordElement, index) => {
+        setTimeout(() => {
+          wordElement.classList.add('slideIn');
+        }, delay);
+        delay += 200; // Adjust the delay timing between words as desired
+      });
+    };
+
+    const animationDelay = 5000; // Delay in milliseconds before starting the animation
+    const animationTimer = setTimeout(() => {
+      animateWords();
+    }, animationDelay);
+
+    return () => {
+      clearTimeout(animationTimer);
+    };
+  }, []);
+
+  return (
+    <div className={styles.container} ref={containerRef}>
+      {words.map((word, index) => (
+        <React.Fragment key={index}>
+          <span className="word">{word}</span>
+          {index !== words.length - 1 && ' '}
+        </React.Fragment>
+      ))}
+    </div>
+  );
+};
+
+export default SlidingWords;
