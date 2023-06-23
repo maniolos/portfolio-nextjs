e120c32f0ba2ef226d7e051b1b27f45b07306b25
maniolos <maniolos8182003@gmail.com>
2023-06-23 09:00:19 +0200

    another one for vercel

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -9,7 +9,6 @@ import useScrollPosition from "./UseScrollHook";
 export default function Portfolio() {
   const [animationStarted, setAnimationStarted] = useState(true);
   const words = ['I', 'strive', 'to', 'create', 'the', 'best', 'UI/UX', 'imaginable'];
-  const target = useRef(null);
   const useScroll = useScrollPosition();
   console.log(useScroll)
   useEffect(() => {
@@ -25,7 +24,7 @@ export default function Portfolio() {
         <h1 className="text-center text-2xl sm:text-3xl w-[20rem] typewriter">Hi my name is..</h1>
         <h1 className="flex justify-center pt-5 font-bold md:text-7xl sm:text-5xl text-4xl bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">Kamil Pawłowski</h1>
         <h1 className="pt-5 justify-center font-mono text-2xl sm:3xl flex-nowrap flex">
-          I'm a <span className="justify-center font-mono text-2xl  sm:3xl flex-nowrap flex px-2 bg-gradient-to-r from-pink-500 to-violet-500 bg-[length:89%_3px] bg-no-repeat bg-bottom">Front-End</span> developer
+          I&apos;m a <span className="justify-center font-mono text-2xl  sm:3xl flex-nowrap flex px-2 bg-gradient-to-r from-pink-500 to-violet-500 bg-[length:89%_3px] bg-no-repeat bg-bottom">Front-End</span> developer
         </h1>
       </div>
       <div className="pt-[2rem]">
